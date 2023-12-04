6cc49bce297f2c65843d7ee5029c56243ab554e4
SliverKeigo <1226355793@qq.com>
2023-12-04 14:10:44 +0800

    😢

---
+++ src/client/pages/main/index.ts
@@ -0,0 +1,16 @@
+import "./index.css";
+
+const nameInput = document.getElementById("nameInput") as HTMLInputElement
+const nameSelect = document.getElementById("nameSelect") as HTMLSelectElement
+const startChat = document.getElementById("startChat") as HTMLButtonElement
+
+startChat.onclick = () => {
+    const userName = nameInput.value
+    const roomName = nameSelect.value
+    if (!roomName || !userName) {
+        return
+    }
+
+    location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+}
+
